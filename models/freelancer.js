7a6590841252f879d99569ad076f344bd7ec596d
Itamar Yarden <itamary9@gmail.com>
2020-12-25 17:28:00 +0200

    fixing routers

--- models/freelancer.js
+++ models/freelancer.js
@@ -1,5 +1,5 @@
 const {Schema, model} = require('mongoose');
-const jobSchema = require('./job');
+const jobsSchema = require('./job');
 const personalDetails = require('./personalDetails')
 
 const skillsSchema = new Schema({
--- routers/freelancer.router.js
+++ routers/freelancer.router.js
@@ -4,10 +4,10 @@ const { freeLancerDbController } = require('../controllers/freelancer.ctrl');
 const freelancerRouter = new Router();
 
 
-freelancerRouter.get('/', freeLancerDbController);              // localhost:3000/api/freelancers
-freelancerRouter.get('/:id', freeLancerDbController);           // localhost:3000/api/freelancers/5
-freelancerRouter.post('/', freeLancerDbController);             // localhost:3000/api/freelancers (with freelancer object)
-freelancerRouter.put('/:id', freeLancerDbController);           // localhost:3000/api/freelancers/38 (with freelancer object)
-freelancerRouter.delete('/:id', freeLancerDbController);        // localhost:3000/api/freelancers/43
+freelancerRouter.get('/', freeLancerDbController.getFreelancers);               // localhost:3000/api/freelancers
+freelancerRouter.get('/:id', freeLancerDbController.getFreelancer);             // localhost:3000/api/freelancers/5
+freelancerRouter.post('/', freeLancerDbController.addFreelancer);               // localhost:3000/api/freelancers         
+freelancerRouter.put('/:id', freeLancerDbController.updateFreelancer);          // localhost:3000/api/freelancers/38 (with freelancer object)
+freelancerRouter.delete('/:id', freeLancerDbController.deleteFreelancer);       // localhost:3000/api/freelancers
 
-module.exports = { freelancerRouter};
+module.exports = { freelancerRouter };
