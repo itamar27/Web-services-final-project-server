cbee2c71198465c9928f778c62d70ccaf48b52f5
Itamar Yarden <itamary9@gmail.com>
2020-12-31 19:50:32 +0200

    final fixes

--- models/job.js
+++ models/job.js
@@ -1,23 +1,23 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const goalSchema = new Schema({
-    phase: {type: Number},
-    name: {type: String},
-    description: {type: String},
-    meaningful: {type: Boolean},
-    comments: {type:String, default:null},
-    progress: {type:Number, default:0}
+    phase: { type: Number },
+    name: { type: String },
+    description: { type: String },
+    meaningful: { type: Boolean },
+    comments: { type: String, default: null },
+    progress: { type: Number, default: 0 }
 });
 
 const jobSchema = new Schema({
-    id: {type: Number},
-    project_name: {type: String},
-    price: {type: String},
-    customer_id: {type: Number},
-    startDate : {type: Date},
-    deadline: {type: Date},
-    goals: [{type: goalSchema}],
-}, {collection: 'jobs'});
+    id: { type: Number },
+    project_name: { type: String },
+    price: { type: String },
+    customer_id: { type: Number },
+    startDate: { type: Date },
+    deadline: { type: Date },
+    goals: [{ type: goalSchema }],
+}, { collection: 'jobs' });
 
 
 const job = model('job', jobSchema);
