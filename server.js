d4354664d15b896fee6d07aaef79219620cfb888
guysh2020 <61948800+guysh2020@users.noreply.github.com>
2021-02-22 23:28:17 +0200

    guy6

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: sessionStore,
-    sameSite: 'none',
-    name: 'connect.sid',
+    // name: 'connect.sid',
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
         secure: true,
+        sameSite: 'lax',
     }
 }));
 // ***** session related *******
