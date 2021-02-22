d8d321ae87a810cf3c9aed83c6ce3c439858d694
guysh2020 <61948800+guysh2020@users.noreply.github.com>
2021-02-22 23:21:11 +0200

    cookie

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: sessionStore,
-    sameSite: null,
-    key: 'connect-sid',
+    sameSite: 'none',
+    name: 'connect.sid',
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
         secure: true,
