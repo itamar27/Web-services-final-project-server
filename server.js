cf9d9c4f55ad5d57fe92ff2effd8db3c03ab1903
guysh2020 <61948800+guysh2020@users.noreply.github.com>
2021-02-22 23:25:38 +0200

    domain

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
         secure: true,
-        domain: 'hungry-mcnulty-42e26a.netlify.app'
     }
 }));
 // ***** session related *******
